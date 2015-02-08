9b9879c578e5ed83ad5221c7f6d1796d50cd89b9
aprillins <aprillins@gmail.com>
2015-02-09 01:00:35 +0700

    Initial Commit

---
+++ filejson.class.php
@@ -0,0 +1,39 @@
+<?php
+
+class FileJSON{
+	private $data;
+	private $filename;
+	private $folder;
+	private $status;
+
+	public function __construct($folder, $filename){
+		$this->folder = $folder;
+		$this->filename = $filename;
+		$this->status = '';
+	}
+
+	public function save(){
+		$file = fopen($this->folder.'/'.$this->filename, 'w'); // Opening file handle
+		fwrite($file, $this->data); // Writing file
+		fclose($file); // Closing file handle
+		$this->status = 'File saved as '. $this->filename;
+		unset($this->data);
+	}
+
+	public function read(){
+		$file = fopen($this->folder.'/'.$this->filename, 'r') or die("Unable to open file!");
+		$content = fread($file,filesize($this->folder.'/'.$this->filename));
+		$content = json_decode($content);
+		return $content;
+	}
+
+	public function setData($data){
+		$this->data = json_encode($data);
+	}
+
+	public function getStatus(){
+		echo $this->status;
+	}
+
+}
+?>
